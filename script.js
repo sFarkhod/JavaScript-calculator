13cee95d536e500ee61e7ce72443bc44ad8bb548
Sokhibov Farkhod <93736788+sFarkhod@users.noreply.github.com>
2021-11-15 10:00:49 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function myFunc() {
     let number2 = parseFloat(document.getElementById('n2').value);
     let operation = document.getElementById('operators').value;
     
+    if (isNaN(number1)) {
+        alert('Inputs cant be empty');
+    }
 
     if (operation === '+') {
         document.getElementById('result').value = number1 + number2;
